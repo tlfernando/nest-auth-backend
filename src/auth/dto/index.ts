86dd520c6d790c9c007b19f3c0dadbecf0e2f2a6
tlfernando <lfernandoproject@gmail.com>
2024-05-23 16:44:05 -0500

    primer commit

---
+++ src/auth/dto/index.ts
@@ -0,0 +1,4 @@
+export * from './create-user.dto';
+export * from './login.dto';
+export * from './register-user';
+export * from './update-auth.dto';
---
+++ src/auth/guards/auth.guard.ts
@@ -0,0 +1,48 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Observable } from 'rxjs';
+import { JwtPayload } from '../interfaces/jwt-payload';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private jwtService: JwtService,
+    private authService: AuthService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+
+    if (!token) {
+      throw new UnauthorizedException('No hay token en la petición');
+    }
+    try {
+      const payload = await this.jwtService.verifyAsync<JwtPayload>(token, {
+        secret: process.env.JWT_SEED,
+      });
+
+      const user = await this.authService.findUserByID(payload.id);
+      if (!user) throw new UnauthorizedException('El usuario no existe');
+      if (!user.isActive)
+        throw new UnauthorizedException('El usuario está inactivo');
+
+      request['user'] = user;
+    } catch (error) {
+      throw new UnauthorizedException();
+    }
+
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers['authorization']?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
